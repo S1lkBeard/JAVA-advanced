8f906a69c6f2d6d9a72c999d21391b21bf44c29a
S1lkBeard <jahwrath@yahoo.com>
2022-08-07 11:58:21 +0300

    Выполненное 1-ое задание

---
+++ Calc.java
@@ -0,0 +1,21 @@
+package JAVA_adv_gr;
+
+import java.util.Scanner;
+
+/**
+ * Created on 05.08.2022
+ *
+ * @author s1lkbeard
+ */
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите дробное число");
+        float pr1 = in.nextFloat();
+        System.out.println("Введите дробное число");
+        float pr2 = in.nextFloat();
+        System.out.printf("%.4f", pr1 + pr2);
+        System.out.println("fdsf");
+    }
+}
