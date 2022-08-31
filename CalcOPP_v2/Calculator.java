f24f20bd17e261b9676d73e861d075f607d30814
S1lkBeard <jahwrath@yahoo.com>
2022-08-31 17:42:32 +0300

    Были добавлены Exception в следующие классы: 1) класс Main - InputMismatchException; 2) класс Calculator - ArithmeticException

--- CalcOPP_v2/Calculator.java
+++ CalcOPP_v2/Calculator.java
@@ -45,6 +45,7 @@ public class Calculator {
 
 
     public double calculation() {
+        double res = 0;
         switch (operation) {
             case "+":
                 return fNumber + sNumber;
@@ -53,7 +54,26 @@ public class Calculator {
             case "*":
                 return fNumber * sNumber;
             case "/":
-                return fNumber / sNumber;
+                //добавил try-catch для деления на 0, но опытным путём установил, что при
+                //использовании переменных, отличных от byte и int, при делении на 0
+                //ошибка ArithmeticException не отлавливается
+                //при использовании переменных других типов (float, double, long)
+                //результатом деления на 0 является "Infinity"
+                //эту странную конструкцию с преобразованием из double в int добавил
+                //для отлавливания ArithmeticException
+                if (sNumber == 0) {
+                    int fNumberInt = (int) fNumber;
+                    int sNumberInt = (int) sNumber;
+                    try {
+                        res = fNumberInt / sNumberInt;
+                    } catch (ArithmeticException e3) {
+                        System.out.println("Делить на ноль можно, но не нужно");
+                        System.out.println("Ошибка: " + e3);
+                    }
+                } else {
+                    res = fNumber / sNumber;
+                }
+                return res;
             default:
                 throw new UnsupportedOperationException("Некорректная операция, Введите +,-,/,*");
         }
--- CalcOPP_v2/Main.java
+++ CalcOPP_v2/Main.java
@@ -1,19 +1,23 @@
 package CalcOPP_v2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите число");
-        double msv0 = in.nextDouble();
-        System.out.println("Введите математический оператор - '+', '-', '*' или '/'");
-        String msv1 = in.next();
-        System.out.println("Введите число");
-        double msv2 = in.nextDouble();
-        //double res = 0;
-        Calculator calc = new Calculator(msv0, msv2, msv1);
-        //res = calc.calculation();
-        System.out.println("Результат вычисления = " + calc.calculation());
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите число");
+            double msv0 = in.nextDouble();
+            System.out.println("Введите математический оператор - '+', '-', '*' или '/'");
+            String msv1 = in.next();
+            System.out.println("Введите число");
+            double msv2 = in.nextDouble();
+            Calculator calc = new Calculator(msv0, msv2, msv1);
+            System.out.println("Результат вычисления = " + calc.calculation());
+        } catch (InputMismatchException e) {
+            System.out.println("Введён некорректный символ. Повторите попытку");
+            System.out.println("Ошибка: " + e);
+        }
     }
 }
